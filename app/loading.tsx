1ca2246ef4008d5075a00c154baf5471d05ab58e
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-01-01 21:00:06 +0200

    404 page and loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,14 @@
-
-
 export default function Loading() {
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <div>
-      <h1 className="text-xl mb-5">Loading...</h1>
-      <span className="loading loading-infinity loading-md"></span>
-      </div>
-    </main>
+    <>
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-primary-content sm:text-5xl">
+            Loading...
+          </h1>
+          <span className="loading loading-infinity loading-md"></span>
+        </div>
+      </main>
+    </>
   );
 }
