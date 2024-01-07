16f5c3fe381e6c58481ad0312c6f9b3c2623bfe3
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-01-07 17:58:36 +0200

    loginform regex

--- app/components/loginForm.tsx
+++ app/components/loginForm.tsx
@@ -2,9 +2,7 @@
 
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react';
-import { useState } from 'react';
-
+import { FormEvent, useState } from 'react';
 
 export default function Form() {
   const router = useRouter();
@@ -14,57 +12,87 @@ export default function Form() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
-    
+
     try {
-    const formData = new FormData(e.currentTarget);
-    const response = await signIn('credentials', {
-      username: formData.get('username'),
-      password: formData.get('password'),
-      redirect: false,
-    });
+      const formData = new FormData(e.currentTarget);
+      const username = formData.get('username')?.toString();
+      const password = formData.get('password')?.toString();
 
-    if (!response?.error) {
-      router.push('/');
-      router.refresh();
-    }
-    else {
-      setError("Invalid credentials");
-    }
-  } catch (error) {
-    console.log({ error });
-    setError("An error occured. Please try again.");
-  } finally {
-    setLoading(false);
-  }
+      // Validate username
+      if (!/^[a-z0-9]{1,20}$/.test(username || '')) {
+        setError('Invalid username');
+        setLoading(false);
+        return;
+      }
 
+      // Validate password
+      if (!/^[a-z0-9]{1,20}$/.test(password || '')) {
+        setError('Invalid password');
+        setLoading(false);
+        return;
+      }
+
+      const response = await signIn('credentials', {
+        username,
+        password,
+        redirect: false
+      });
+
+      if (!response?.error) {
+        router.push('/');
+        router.refresh();
+      } else {
+        setError('Invalid credentials');
+      }
+    } catch (error) {
+      console.log({ error });
+      setError('An error occured. Please try again.');
+    } finally {
+      setLoading(false);
+    }
   };
-  return (  
-    <form
-      onSubmit={handleSubmit}
-      className="space-y-4 md:space-y-6"
-    >
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
       <input
         className="input input-bordered w-full max-w-xs"
         name="username"
         type="text"
-        placeholder='Username'
+        placeholder="Username"
         required
+        maxLength={20}
       />
       <input
         className="input input-bordered w-full max-w-xs"
         name="password"
         type="password"
-        placeholder='Password'
+        placeholder="Password"
         required
+        maxLength={20}
       />
       <button className="btn btn-primary" type="submit" disabled={loading}>
-      {loading ? <span className="loading loading-spinner"></span> : 'SIGN IN'}
-        </button>
-        {error && (
+        {loading ? (
+          <span className="loading loading-spinner"></span>
+        ) : (
+          'SIGN IN'
+        )}
+      </button>
+      {error && (
         <div role="alert" className="alert alert-error">
-        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>{error}</span>
-      </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{error}</span>
+        </div>
       )}
     </form>
   );
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,11 +1,13 @@
-
 export default async function IndexPage() {
-
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <div>
         <h1 className="text-xl mb-5">Hello from playground</h1>
-        </div>
+        <p className="text-gray-500">
+          This is a playground page. You can use it to test out new components
+          and ideas.
+        </p>
+      </div>
     </main>
   );
 }
