d68325ecc34a1e7436b67678a8f25fe16e3de982
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-01-10 20:39:46 +0200

    tabs fix

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -12,7 +12,7 @@ export default function IndexPage() {
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <div>
         <h1 className="text-xl mb-5">Hello from playground</h1>
-        <p className="text-gray-500">
+        <p className="text-gray-500 mb-5">
           This is a playground page. You can use it to test out new components
           and ideas.
         </p>
@@ -23,12 +23,15 @@ export default function IndexPage() {
             role="tab"
             className="tab"
             aria-label="Tab 1"
-            onClick={() => handleTabChange(0)}
-            checked
+            id="tab1"
+            checked={tab === 0}
+            onChange={() => handleTabChange(0)}
           />
           <div
             role="tabpanel"
-            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+            className={`tab-content bg-base-100 border-base-300 rounded-box p-6 ${
+              tab === 0 ? 'block' : 'hidden'
+            }`}
           >
             Tab content 1
           </div>
@@ -39,11 +42,16 @@ export default function IndexPage() {
             role="tab"
             className="tab"
             aria-label="Tab 2"
-            onClick={() => handleTabChange(1)}
+            id="tab2"
+            checked={tab === 1}
+            onChange={() => handleTabChange(1)}
           />
+
           <div
             role="tabpanel"
-            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+            className={`tab-content bg-base-100 border-base-300 rounded-box p-6 ${
+              tab === 1 ? 'block' : 'hidden'
+            }`}
           >
             Tab content 2
           </div>
@@ -54,11 +62,16 @@ export default function IndexPage() {
             role="tab"
             className="tab"
             aria-label="Tab 3"
-            onClick={() => handleTabChange(2)}
+            id="tab3"
+            checked={tab === 2}
+            onChange={() => handleTabChange(2)}
           />
+
           <div
             role="tabpanel"
-            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+            className={`tab-content bg-base-100 border-base-300 rounded-box p-6 ${
+              tab === 2 ? 'block' : 'hidden'
+            }`}
           >
             Tab content 3
           </div>
