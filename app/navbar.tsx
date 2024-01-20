072f7e4e6daf87fc10af87ce7232b6d6d168aa8e
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-01-20 11:35:47 +0200

    show navbar only when in session

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -73,57 +73,52 @@ export default function Navbar({ user }: { user: any }) {
                   ))}
                 </div>
               </div>
-              {user && (
-                <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                  <div className="avatar placeholder">
-                    <div className="text-primary-content bg-primary mask-square h-8 w-20">
-                      <span className="text-base">{user.name}</span>
-                    </div>
+              <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                <div className="avatar placeholder">
+                  <div className="text-primary-content bg-primary mask-square h-8 w-20">
+                    <span className="text-base">{user.name}</span>
                   </div>
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="flex rounded-full text-sm hover:text-accent">
-                        <span className="sr-only">Open user menu</span>
-                        <UserCircleIcon
-                          className="h-8 w-8"
-                          aria-hidden="true"
-                        />
-                      </Menu.Button>
-                    </div>
-
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {user && (
-                          <Menu.Item>
-                            {({ active }) => (
-                              <button
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'flex w-full px-4 py-2 text-sm text-gray-700'
-                                )}
-                                onClick={async () => {
-                                  await signOut();
-                                  router.push('/');
-                                }}
-                              >
-                                Sign out
-                              </button>
-                            )}
-                          </Menu.Item>
-                        )}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
                 </div>
-              )}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full text-sm hover:text-accent">
+                      <span className="sr-only">Open user menu</span>
+                      <UserCircleIcon className="h-8 w-8" aria-hidden="true" />
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      {user && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'flex w-full px-4 py-2 text-sm text-gray-700'
+                              )}
+                              onClick={async () => {
+                                await signOut();
+                                router.push('/');
+                              }}
+                            >
+                              Sign out
+                            </button>
+                          )}
+                        </Menu.Item>
+                      )}
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
               <div className="-mr-2 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center square bg-gray-100 p-2 text-neutral hover:bg-gray-100 hover:text-accent">
                   <span className="sr-only">Open main menu</span>
@@ -156,33 +151,31 @@ export default function Navbar({ user }: { user: any }) {
               ))}
             </div>
             <div className="border-t-2 border-primary-content pt-4 pb-3">
-              {user && (
-                <>
-                  <div className="flex items-center px-4">
-                    <div className="flex-shrink-0">
-                      <UserCircleIcon className="h-8 w-8" aria-hidden="true" />
-                    </div>
-                    <div className="ml-3">
-                      <div className="avatar placeholder">
-                        <div className="text-primary-content bg-primary mask-square h-8 w-20">
-                          <span className="text-base">{user.name}</span>
-                        </div>
+              <>
+                <div className="flex items-center px-4">
+                  <div className="flex-shrink-0">
+                    <UserCircleIcon className="h-8 w-8" aria-hidden="true" />
+                  </div>
+                  <div className="ml-3">
+                    <div className="avatar placeholder">
+                      <div className="text-primary-content bg-primary mask-square h-8 w-20">
+                        <span className="text-base">{user.name}</span>
                       </div>
                     </div>
                   </div>
-                  <div className="mt-3 space-y-1">
-                    <button
-                      onClick={async () => {
-                        await signOut();
-                        router.push('/');
-                      }}
-                      className="block px-4 py-2 text-base font-medium text-primary-content hover:bg-primary-content hover:text-neutral"
-                    >
-                      Sign out
-                    </button>
-                  </div>
-                </>
-              )}
+                </div>
+                <div className="mt-3 space-y-1">
+                  <button
+                    onClick={async () => {
+                      await signOut();
+                      router.push('/');
+                    }}
+                    className="block px-4 py-2 text-base font-medium text-primary-content hover:bg-primary-content hover:text-neutral"
+                  >
+                    Sign out
+                  </button>
+                </div>
+              </>
             </div>
           </Disclosure.Panel>
         </>
