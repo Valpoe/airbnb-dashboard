d37bc138ec187d491e0aec7a3d7e15b946bc3699
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-01-25 17:41:29 +0200

    component tests

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -19,6 +19,17 @@ export default async function IndexPage() {
         <Suspense fallback={<StatisticsSkeleton />}>
           <DataTabs />
         </Suspense>
+        <div className="flex flex-col gap-4 w-52">
+          <div className="skeleton h-32 w-full"></div>
+          <div className="skeleton h-4 w-28"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+        </div>
+        <div className="avatar">
+          <div className="w-24 rounded">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
       </div>
     </main>
   );
