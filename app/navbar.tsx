2212390fe669126e7b0c49a99d1d14d39828b7b9
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-04-22 21:33:16 +0300

    style navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -34,27 +34,27 @@ export default function Navbar({ user }: { user: any }) {
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
                   <Link href="/monthly-report">
-                  <svg
-                    width="32"
-                    height="32"
-                    viewBox="0 0 32 32"
-                    fill="none"
-                    className="text-gray-100"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      width="100%"
-                      height="100%"
-                      rx="16"
-                      fill="currentColor"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                      fill="black"
-                    />
-                  </svg>
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      className="text-gray-100"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        width="100%"
+                        height="100%"
+                        rx="16"
+                        fill="currentColor"
+                      />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                        fill="black"
+                      />
+                    </svg>
                   </Link>
                 </div>
                 <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
@@ -121,7 +121,7 @@ export default function Navbar({ user }: { user: any }) {
                   </Transition>
                 </Menu>
               </div>
-              <div className="-mr-2 flex items-center sm:hidden">
+              <div className="flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center square bg-gray-100 p-2 text-neutral hover:bg-gray-100 hover:text-accent">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -142,7 +142,7 @@ export default function Navbar({ user }: { user: any }) {
                   href={item.href}
                   className={classNames(
                     pathname === item.href
-                      ? 'bg-primary-content w-48 border-neutral text-accent'
+                      ? 'bg-primary-content w-48 border-neutral text-neutral'
                       : 'border-transparent w-48 text-primary-content hover:bg-primary-content hover:border-neutral hover:text-neutral',
                     'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
                   )}
@@ -172,7 +172,7 @@ export default function Navbar({ user }: { user: any }) {
                       await signOut();
                       router.push('/');
                     }}
-                    className="block px-4 py-2 text-base font-medium text-primary-content hover:bg-primary-content hover:text-neutral"
+                    className="block px-4 py-2 text-base font-medium text-primary-content hover:bg-primary-content hover:text-neutral ml-1"
                   >
                     Sign out
                   </button>
