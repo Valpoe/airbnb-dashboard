c8044e623ecce2c910eac7c8d5f84cd93ed8a91e
Valpoe <anders.tuomainen@edu.savonia.fi>
2024-05-10 18:32:38 +0300

    add line chart

--- app/ui/reservations-data/line-chart.tsx
+++ app/ui/reservations-data/line-chart.tsx
@@ -7,8 +7,7 @@ import {
 import { calculateAmountOfDays, getRandomColor } from '@/app/lib/utils';
 import 'chart.js/auto';
 import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
-import dayjs from 'dayjs';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import {
   UIBackground,
   ChartXY,
@@ -22,7 +21,6 @@ import {
   lightningChart,
   Themes
 } from '@arction/lcjs';
-import { Line } from 'react-chartjs-2';
 import { createChart } from '@/app/components/create-chart';
 
 export default function LineChart({
@@ -39,23 +37,23 @@ export default function LineChart({
   const [chart, setChart] = useState<ChartXY<UIBackground> | undefined>(
     undefined
   );
+  const [chartData, setChartData] = useState<any>(undefined);
+  console.log('ChartData', chartData);
+  const [selectedDataType, setSelectedDataType] =
+    useState<DataTypeKey>('amount');
+
+  const toggleDataType = (dataType: DataTypeKey) => {
+    setSelectedDataType(dataType);
+  };
 
   useEffect(() => {
     const licenseKey = process.env.NEXT_PUBLIC_LC_KEY;
 
     if (licenseKey) {
       const lc = createChart(licenseKey, 'chart-container');
-      lc.getDefaultAxisX()
-        .setTickStrategy(AxisTickStrategies.DateTime)
-        .setTitle('Title X')
-        .setInterval({
-          start: new Date(2022, 0, 1).getTime(),
-          end: new Date(2022, 0, 31).getTime()
-        });
-      lc.getDefaultAxisY().setTitle('Title Y');
-      lc.setTitle('Title Chart');
+      lc.getDefaultAxisX().setTickStrategy(AxisTickStrategies.DateTime);
+      lc.setTitle('');
       setChart(lc);
-
       return () => {
         if (lc) {
           lc.dispose();
@@ -68,167 +66,67 @@ export default function LineChart({
     }
   }, []);
 
-  const diesel = [
-    { x: new Date(2022, 0, 1).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 2).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 3).getTime(), y: 1.58 },
-    { x: new Date(2022, 0, 4).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 5).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 6).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 7).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 8).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 9).getTime(), y: 2.26 },
-    { x: new Date(2022, 0, 10).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 11).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 12).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 13).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 14).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 15).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 16).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 17).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 18).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 19).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 20).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 21).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 22).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 23).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 24).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 25).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 26).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 27).getTime(), y: 1.5 },
-    { x: new Date(2022, 0, 28).getTime(), y: 1.5 },
-    { x: new Date(2022, 0, 29).getTime(), y: 1.5 }
-  ];
+  useEffect(() => {
+    const getStatisticsForSelectedListings = () => {
+      const datasets = selectedListings.map((selectedListingId) => {
+        const listingReservations = reservations
+          .filter((reservation) => reservation.listing_id === selectedListingId)
+          .sort(
+            (a, b) =>
+              new Date(a.payout_date).getTime() -
+              new Date(b.payout_date).getTime()
+          );
 
-  const lineSeries = chart?.addLineSeries().setName('Diesel');
-  lineSeries?.add(diesel);
+        let cumulativeAmount = 0;
+        const data = listingReservations.map((reservation) => {
+          let totalValue;
+          if (dataTypes[selectedDataType].label === 'Reservations') {
+            totalValue = 1;
+          } else if (dataTypes[selectedDataType].label === 'Occupancy Rate') {
+            const totalNights = reservation.nights;
+            totalValue =
+              (totalNights /
+                calculateAmountOfDays(dateRange.startDate, dateRange.endDate)) *
+              100;
+          } else {
+            totalValue = reservation[dataTypes[selectedDataType].property];
+          }
 
-  chart
-    ?.getDefaultAxisY()
-    .setTitle('$/litre')
-    .setInterval({ start: 0, end: 3, stopAxisAfter: true });
+          cumulativeAmount += typeof totalValue === 'number' ? totalValue : 0;
+          return {
+            x: new Date(reservation.payout_date).getTime(),
+            y: cumulativeAmount
+          };
+        });
 
-  // Enable AutoCursor auto-fill.
-  chart?.setAutoCursor((cursor) =>
-    cursor
-      .setResultTableAutoTextStyle(true)
-      .setTickMarkerXVisible(false)
-      .setTickMarkerYAutoTextStyle(true)
-  );
-
-  // if (chart) {
-  //   const series = chart.addLineSeries({
-  //     dataPattern: { pattern: 'ProgressiveX' }
-  //   });
-  //   series.add([
-  //     { x: 0, y: 0 },
-  //     { x: 1, y: 1 },
-  //     { x: 2, y: 2 },
-  //     { x: 3, y: 3 },
-  //     { x: 4, y: 4 }
-  //   ]);
-  // }
-
-  console.log('Chart', chart);
-
-  const [selectedDataType, setSelectedDataType] =
-    useState<DataTypeKey>('amount');
-
-  const toggleDataType = (dataType: DataTypeKey) => {
-    setSelectedDataType(dataType);
-  };
-
-  const getStatisticsForSelectedListings = () => {
-    return selectedListings.map((selectedListingId) => {
-      const listingReservations = reservations
-        .filter((reservation) => reservation.listing_id === selectedListingId)
-        .sort(
-          (a, b) =>
-            new Date(a.payout_date).getTime() -
-            new Date(b.payout_date).getTime()
-        );
-
-      let cumulativeAmount = 0;
-
-      const data = listingReservations.map((reservation) => {
-        let totalValue;
-
-        if (dataTypes[selectedDataType].label === 'Reservations') {
-          // For 'Reservations' type, calculate the count of rows
-          totalValue = 1;
-        } else if (dataTypes[selectedDataType].label === 'Occupancy Rate') {
-          // For 'Occupancy Rate' type, calculate the occupancy rate
-          const totalNights = reservation.nights;
-          totalValue =
-            (totalNights /
-              calculateAmountOfDays(dateRange.startDate, dateRange.endDate)) *
-            100;
-        } else {
-          totalValue = reservation[dataTypes[selectedDataType].property];
-        }
-
-        cumulativeAmount += typeof totalValue === 'number' ? totalValue : 0;
-
-        return {
-          x: dayjs(reservation.payout_date).format('YYYY-MM-DD'),
-          y: cumulativeAmount
-        };
+        return data;
       });
 
-      const startDate = new Date(listingReservations[0]?.payout_date);
-      const endDate = new Date(
-        listingReservations[listingReservations.length - 1]?.payout_date
-      );
+      setChartData(datasets.flat());
+    };
 
-      return {
-        label: listings.find((listing) => listing.id === selectedListingId)
-          ?.internal_name,
-        data,
-        fill: false,
-        borderColor: getRandomColor(),
-        startDate,
-        endDate
-      };
-    });
-  };
+    getStatisticsForSelectedListings();
+  }, [reservations, listings, selectedListings, dateRange, selectedDataType]);
 
-  const datasets = getStatisticsForSelectedListings();
+  useEffect(() => {
+    if (chart && chartData) {
+      chart.getSeries().forEach((series) => series.dispose());
+      chart.getDefaultAxisY().setTitle(dataTypes[selectedDataType].label);
 
-  const data = {
-    datasets
-  };
+      const startMillis = new Date(dateRange.startDate).getTime();
+      const endMillis = new Date(dateRange.endDate).getTime();
+      chart
+        .getDefaultAxisX()
+        .setTitle('Date Range')
+        .setInterval({ start: startMillis, end: endMillis });
 
-  const options = {
-    responsive: true,
-    pointStyle: false,
-    plugins: {
-      legend: {
-        position: 'top' as const
-      }
-    },
-    scales: {
-      x: {
-        type: 'time' as const,
-        time: {
-          unit: 'day' as const,
-          displayFormats: {
-            day: 'MMM D'
-          },
-          tooltipFormat: 'MMMM D, YYYY'
-        },
-        title: {
-          display: true,
-          text: 'Date Range'
-        }
-      },
-      y: {
-        title: {
-          display: true,
-          text: dataTypes[selectedDataType].label
-        }
-      }
+      const lineSeries = chart
+        .addLineSeries({ dataPattern: { pattern: 'ProgressiveX' } })
+        .setStrokeStyle((strokeStyle) => strokeStyle.setThickness(2));
+
+      lineSeries.add(chartData);
     }
-  };
+  }, [chart, chartData]);
 
   return (
     <div className="flex flex-wrap justify-center">
