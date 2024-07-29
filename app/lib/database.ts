92ad425192976d658ed3c8878c963a73f07a1404
Valpoe <valpoe@proton.me>
2024-07-29 10:59:35 +0300

    remove reduntant fetches

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -4,37 +4,6 @@ import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
 import { Listing, Reservation } from './definitions';
 
-export async function fetchAllReservations() {
-  try {
-    const result = await sql<Reservation>`
-    SELECT * FROM airbnb_data
-    ORDER BY payout_date DESC
-  `;
-    const data = result.rows;
-    return data;
-  } catch (error) {
-    console.log('Database error: ', error);
-    throw new Error('Error fetching reservations');
-  }
-}
-
-export async function fetchReservationsByDateRange(
-  startDate: string,
-  endDate: string
-) {
-  try {
-    const result = await sql<Reservation>`
-    SELECT * FROM airbnb_data
-    WHERE payout_date >= ${startDate} AND payout_date <= ${endDate}
-  `;
-    const data = result.rows;
-    return data;
-  } catch (error) {
-    console.log('Database error: ', error);
-    throw new Error('Error fetching reservations');
-  }
-}
-
 export async function fetchListings() {
   noStore();
   try {
